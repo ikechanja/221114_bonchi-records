c6dbb59ca6b5e6b1b696702b7b2625f5072bb0c2
ikechanja <ikejiri.asana@gmail.com>
2022-11-14 17:56:24 +0900

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { defineNuxtConfig } from 'nuxt'
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content'
+  ],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
+})
